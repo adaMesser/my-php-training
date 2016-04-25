72f9740e107aec16ae616bd6f567186fcce56da1
LizabethEpifanova <ladydowaakhin@gmail.com>
2016-04-25 17:11:12 +0300

    Change min_value to value, add comparison_sign to option

--- index.php
+++ index.php
@@ -4,9 +4,10 @@ require_once('results.php');
 
 $options = array(
     'dice_type' => isset($_POST['facet']) ? $_POST['facet'] : 6,
-    'min_value' => isset($_POST['min_value']) ? $_POST['min_value'] : 1,
+    'value' => isset($_POST['value']) ? $_POST['value'] : 1,
     'count' => isset($_POST['throw']) ? $_POST['throw']: 0,
     'trigger' => isset($_POST['trigger']) ? $_POST['trigger'] : 0,
+    'comparison_sign' => isset($_POST['comparison_sign']) ? $_POST['comparison_sign'] : '',
     'command' =>isset($_POST['command']) ? $_POST['command'] : '',
 );
 
